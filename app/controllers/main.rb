33890f40c2b3f1dde511a94745f57e2c6daaedbd
Promoveo <zappeln@gmail.com>
2014-01-24 15:47:35 -0600

    work it

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -5,19 +5,36 @@ require_relative '../view/viewer'
 
 
 case ARGV[0]
-when nil
-  ListView.display_error
-when "list"
-  task_list = List.list_all(ARGV[1])
-  ListView.header
-  ListView.display_list(task_list)
-when "add"
-  Item.add(ARGV[1], ARGV[2])
-  ListView.add_message(ARGV[2])
-when "delete"
-  Item.delete(ARGV[1])
-  ListView.delete_message(ARGV[1])
-when "complete"
-  Item.complete(ARGV[1])
-  ListView.check_message(ARGV[1])
+  when "list_items"
+    task_list = List.list_all(ARGV[1])
+    ListView.header
+    ListView.display_list(task_list)
+  when "add_item"
+    Item.add(ARGV[1], ARGV[2])
+    ListView.add_message(ARGV[2])
+  when "delete_item"
+    Item.delete(ARGV[1])
+    ListView.delete_message(ARGV[1])
+  when "complete_item"
+    Item.complete(ARGV[1])
+    ListView.check_message(ARGV[1])
+
+
+
+
+
+  when "list_lists"
+    all_lists= List.lists
+    ListView.header
+    ListView.display_lists(all_lists)
+  when "add_list"
+    List.add(ARGV[1])
+    ListView.add_list_message(ARGV[1])
+  when "delete_list"
+    List.delete(ARGV[1])
+    ListView.delete_message(ARGV[1])
+  when "help"
+
+  else
+    ListView.display_error
 end
