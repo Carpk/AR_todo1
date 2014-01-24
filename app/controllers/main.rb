3a5c12ee43f1a7ed1a69fa5bbe31a46bff44dfa9
Promoveo <zappeln@gmail.com>
2014-01-24 16:31:57 -0600

    delete doesn't work yet

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -6,35 +6,31 @@ require_relative '../view/viewer'
 
 case ARGV[0]
   when "list_items"
-    task_list = List.list_all(ARGV[1])
+    item_list = List.list_all(ARGV[1])
     ListView.header
-    ListView.display_list(task_list)
+    ListView.display_items(item_list)
   when "add_item"
     Item.add(ARGV[1], ARGV[2])
-    ListView.add_message(ARGV[2])
+    ListView.add_item_message(ARGV[2])
   when "delete_item"
     Item.delete(ARGV[1])
-    ListView.delete_message(ARGV[1])
+    ListView.delete_item_message(ARGV[1])
   when "complete_item"
     Item.complete(ARGV[1])
-    ListView.check_message(ARGV[1])
+    ListView.complete_item_message(ARGV[1])
 
-
-
-
-
-  when "list_lists"
+  when "show_lists"
     all_lists= List.lists
     ListView.header
-    ListView.display_lists(all_lists)
+    ListView.show_lists(all_lists)
   when "add_list"
     List.add(ARGV[1])
     ListView.add_list_message(ARGV[1])
   when "delete_list"
     List.delete(ARGV[1])
-    ListView.delete_message(ARGV[1])
+    ListView.delete_list_message(ARGV[1])
   when "help"
-
+    ListView.display_help
   else
     ListView.display_error
 end
